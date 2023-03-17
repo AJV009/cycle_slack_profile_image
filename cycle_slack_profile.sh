bad3f711bb492b92caf0a7ad16d296f980726a5d
Alphons Jaimon <jaimonalphons@gmail.com>
2023-03-17 15:38:04 +0530

    CSP-00: Add requirements.txt

--- cycle_slack_profile.sh
+++ cycle_slack_profile.sh
@@ -1,10 +1,16 @@
-if pgrep python3 | xargs ps | grep cycle_slack_profile > /dev/null
+if pgrep python | xargs ps | grep cycle_slack_profile > /dev/null
 then
     echo "Slack profile updater already running"
     exit
 else
     echo "Starting Slack profile updater in the background..."
-    source ~/miniconda3/etc/profile.d/conda.sh;
-    conda activate slackupdater; 
-    python3 ~/project/personal/cycle_slack_profile_image/cycle_slack_profile.py &
+    if [ -x "$(command -v mkvirtualenv)" ]; then
+        if workon cycle_slack_profile > /dev/null; then
+            workon cycle_slack_profile
+        else
+            mkvirtualenv cycle_slack_profile
+            pip install -r ~/project/personal/cycle_slack_profile_image/requirements.txt
+        fi
+    fi
+    python ~/project/personal/cycle_slack_profile_image/cycle_slack_profile.py &
 fi
